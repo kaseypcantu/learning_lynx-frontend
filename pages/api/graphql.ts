55054512511a3eb4c25fd08630086aafea6b8b02
KaseyCantu-SE <kasey.cantu@shipengine.com>
2020-11-22 12:12:05 -0600

    Removed hello.tsx and replaced it with hello.ts. I also added a GraphQL endpoint to allow the frontend server to call the GraphQL API server.

---
+++ pages/api/graphql.ts
@@ -0,0 +1,25 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function graphql(
+  req: NextApiRequest,
+  res: NextApiResponse
+): Promise<void> {
+
+  const headers = new Headers();
+
+  headers.append('Content-Type', 'application/json');
+
+  try {
+    const response = await fetch('http://localhost:1993/graphql', {
+      method: 'POST',
+      headers: headers,
+      body: JSON.stringify(req.body),
+    });
+
+    const responseJSON = await response.json();
+    res.status(response.status).json(responseJSON);
+  } catch (error) {
+    console.error(error);
+    res.status(error.status || 500).end(error.message);
+  }
+}

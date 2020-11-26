6fa1b53b384845ce72afb74fd6e2ac880745c085
kaseypcantu <kaseypaulcantu@gmail.com>
2020-11-26 01:03:10 -0600

    Created form validation and validation messaging via Yup

---
+++ utils/constants/yupMessaging.ts
@@ -0,0 +1,12 @@
+// Yup validation messaging model
+
+export enum YupMessaging {
+  required = 'Field is required',
+  min = 'Min: ${min} characters',
+  max = 'Max: ${max} characters',
+  linkTitleRequired = 'Title is required',
+  linkUrl = 'Must be a valid URL',
+  linkUrlRequired = 'URL required',
+  descriptionRequired = 'Description is required',
+  programmingLanguageRequired = 'Language is required'
+}

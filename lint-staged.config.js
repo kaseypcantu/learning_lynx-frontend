701c7667c1a60f6c5119dd49ed51de5ff678bb60
KaseyCantu-SE <kasey.cantu@shipengine.com>
2020-12-02 16:12:21 -0600

    Updated key in lint-staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,12 @@
 module.exports = {
-  '**/*.ts?(x)': [
-    () => 'yarn tsc:check',
-    // () => 'yarn gql:gen'
-  ],
+  linters: {
+    '**/*.(js|jsx|ts|tsx|md|css|sass|scss|graphql|yml|yaml|json)': [
+      () => 'yarn tsc:check',
+      () => 'eslint --fix',
+      () => 'prettier --write',
+      () => 'git add -v'
+      // () => 'yarn gql:gen'
+    ],
+  }
 };
+// TODO: GET LINT STAGED WORKING WITH HUSKY HOOKS

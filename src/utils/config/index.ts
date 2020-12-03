fb8459ee1af413dc4de7e64addb7619ab7a95bd0
kaseypcantu <kaseypaulcantu@gmail.com>
2020-12-02 21:30:05 -0600

    Added a config file that pulls env vars and manages critical vars.

--- src/utils/config/index.ts
+++ src/utils/config/index.ts
@@ -1,3 +1,4 @@
 export const isProd = process.env.NODE_ENV === 'production';
-const isDev = process.env.NODE_ENV === 'development';
+// export const isDev = process.env.NODE_ENV === 'development';
 export const GRAPHQL_SERVER = process.env.GRAPHQL_SERVER as string;
+export const NGROK_SUBDOMAIN = process.env.NGROK_SUBDOMAIN;

6d49ea4822c0531dc97eb692fbc53c4d4d2bef9a
kaseypcantu <kaseypaulcantu@gmail.com>
2020-12-02 21:50:31 -0600

    Removed unused import 🛠

--- src/pages/api/graphql.ts
+++ src/pages/api/graphql.ts
@@ -2,20 +2,17 @@ import 'dotenv/config';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { GRAPHQL_SERVER, isProd } from '../../utils/config';
 
-export default async function graphql(req: NextApiRequest, res: NextApiResponse): Promise<void> {
+async function graphql(req: NextApiRequest, res: NextApiResponse): Promise<void> {
   const headers = new Headers();
 
   headers.append('Content-Type', 'application/json');
 
   try {
-    const response = await fetch(
-      isProd ? (GRAPHQL_SERVER as string ?? 'http://localhost:1993/graphql') : 'http://localhost:1993/graphql',
-      {
-        method: 'POST',
-        headers: headers,
-        body: JSON.stringify(req.body),
-      }
-    );
+    const response = await fetch(isProd ? GRAPHQL_SERVER : 'http://localhost:1993/graphql', {
+      method: 'POST',
+      headers: headers,
+      body: JSON.stringify(req.body),
+    });
 
     const responseJSON = await response.json();
     console.log(res.status(response.status).json(responseJSON));
@@ -25,3 +22,5 @@ export default async function graphql(req: NextApiRequest, res: NextApiResponse)
     res.status(error.status || 500).end(error.message);
   }
 }
+
+export default graphql;

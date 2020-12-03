a569b8a7bcc8847ffdb3177ff68b5753552a73be
kaseypcantu <kaseypaulcantu@gmail.com>
2020-12-02 19:43:40 -0600

    Updated with Prettier formatting applied.

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,3 @@
 module.exports = {
-  plugins: [
-    'postcss-import',
-    'tailwindcss',
-    'postcss-preset-env'
-  ]
-}
+  plugins: ['postcss-import', 'tailwindcss', 'postcss-preset-env'],
+};

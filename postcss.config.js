2256549887aad5f16f475cb8e78f1bec0df74252
KaseyCantu-SE <kasey.cantu@shipengine.com>
2020-12-06 00:13:17 -0600

    Adjusted formatting in postcss.config.js :hammer:

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  plugins: ["autoprefixer", 'postcss-import', 'tailwindcss', 'postcss-preset-env'],
+  plugins: ['autoprefixer', 'postcss-import', 'tailwindcss', 'postcss-preset-env'],
 };

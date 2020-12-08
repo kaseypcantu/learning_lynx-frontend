abf1eb1311ee3de4421d689da4dae84f458bd53d
kaseypcantu <kaseypaulcantu@gmail.com>
2020-12-08 01:17:32 -0600

    Updated eslint rules :gear:

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,8 +1,10 @@
+// https://github.com/okonet/lint-staged#example-run-tsc-on-changes-to-typescript-files-but-do-not-pass-any-filename-arguments
 module.exports = {
   '**/*.(js|jsx|ts|tsx)+': [
     // () => 'yarn gql:gen',
     (filenames) => filenames.map((filename) => `prettier --write '${filename}'`),
-    () => 'eslint --fix',
+    (filenames) => (filenames.length ? 'eslint --fix .' : `eslint --fix ${filenames.join(' ')}`),
+    // () => 'eslint --fix .',
     () => 'yarn tsc:check',
     // () => 'git add -v',
   ],
